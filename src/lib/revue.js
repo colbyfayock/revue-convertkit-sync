fb1d87bcdbd192ed16e4fb60a1760eefcc024281
colbyfayock <fayock@gmail.com>
2021-12-31 09:08:56 -0500

    error handling

--- src/lib/revue.js
+++ src/lib/revue.js
@@ -5,15 +5,20 @@ const fetch = require('node-fetch');
  */
 
 async function getSubscribers() {
-  const response = await fetch('https://www.getrevue.co/api/v2/subscribers', {
-    headers: {
-      Authorization: `Token ${process.env.REVUE_API_KEY}`
+  try {
+    const response = await fetch('https://www.getrevue.co/api/v2/subscribers', {
+      headers: {
+        Authorization: `Token ${process.env.REVUE_API_KEY}`
+      }
+    });
+    if ( `${response.status}`.substr(0, 1) !== '2' ) {
+      throw new Error(response.statusText)
     }
-  });
-
-  const subscribers = await response.json();
-
-  return subscribers;
+    const subscribers = await response.json();
+    return subscribers;
+  } catch(e) {
+    throw e;
+  }
 }
 
 module.exports.getSubscribers = getSubscribers;
